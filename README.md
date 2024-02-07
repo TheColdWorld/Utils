0af2fa23b8a021d5f92ed9d1f84833c29285884d
TheColdWorld <3264524796@qq.com>
2024-02-08 03:23:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+a simple util for TheColdWorld
+it  will not publish any on github release!
---
+++ src/Utils/Utils.cs
@@ -0,0 +1,17 @@
+﻿namespace TheColdWorld.Utils;
+
+
+public static class Utils
+{
+    /// <typeparam name="T">any class</typeparam>
+    /// <param name="item">the object that be ckecked null</param>
+    /// <returns>item</returns>
+    /// <exception cref="ArgumentNullException">if item is null</exception>
+    public static T RequirsNotNull<T>(T? item) => item is null ? throw new ArgumentNullException(nameof(item)) : item;
+    /// <typeparam name="T">any class</typeparam>
+    /// <param name="item">the object that be ckecked null</param>
+    /// <param name="Default">the return object if item is null</param>
+    /// <returns>item or Default (if item is null)</returns>
+    public static T RequirsNotNullOrDefault<T>(T? item, T Default) => item is null ? Default : item;
+}
+
