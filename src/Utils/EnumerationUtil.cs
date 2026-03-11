ed3b120fa7557035bb3d8d6746393da9b41c85dd
TheColdWorld <3264524796@qq.com>
2026-03-12 00:01:29 +0800

    fix:doc missing

--- src/Utils/EnumerationUtil.cs
+++ src/Utils/EnumerationUtil.cs
@@ -91,7 +91,7 @@ public static class EnumerationUtil
     /// get the first item from <paramref name="source"/> or the <see langword="value"/> of <paramref name="orElseGetter"/>
     /// </summary>
     /// <param name="orElseGetter">The getter if <paramref name="source"/> is empty</param>
-    /// <returns></returns>
+    /// <returns>First item of <paramref name="source"/> if exists ,otherwise <paramref name="orElseGetter"/>.Invoke()</returns>
     public static TResult FirstOrDefault<TResult>(this IEnumerable<TResult> source, Func<TResult> orElseGetter)
     {
         var rst = source.TryGetFirst(out bool present);
@@ -101,10 +101,103 @@ public static class EnumerationUtil
     /// get the last item from <paramref name="source"/> or the <see langword="value"/> of <paramref name="orElseGetter"/>
     /// </summary>
     /// <param name="orElseGetter">The getter if <paramref name="source"/> is empty</param>
-    /// <returns></returns>
+    /// <returns>Last item of <paramref name="source"/> if exists ,otherwise <paramref name="orElseGetter"/>.Invoke()</returns>
     public static TResult LastOrDefault<TResult>(this IEnumerable<TResult> source, Func<TResult> orElseGetter)
     {
         var rst = source.TryGetLast(out bool present);
         return !present ? orElseGetter() : rst!;
+    }
+    /// <summary>
+    /// get the index in <paramref name="item"/> that size in <see cref="long"/>
+    /// </summary>
+    /// <param name="src">source of item</param>
+    /// <param name="item">the item that will find index in <paramref name="src"/></param>
+    /// <returns>a <see cref="long"/>(value >= 0) about <paramref name="item"/>'s index of <paramref name="src"/>,otherwise <see langword="-1"/></returns>
+    /// <seealso cref="LongIndexesOf"/>
+    public static long LongIndexOf<T>(this IEnumerable<T> src, ref T item)
+    {
+        IEnumerator<T> enumerator = src.GetEnumerator();
+        enumerator.MoveNext();
+        long index = 0;bool found = false;
+        do
+        {
+            if (object.ReferenceEquals(item, enumerator.Current)) break;
+            index++;
+        }
+        while (enumerator.MoveNext());
+        return found ? index : -1;
+    }
+    public static long[] LongIndexesOf<T>(this IEnumerable<T> src, T[] items) {
+        long[] result = new long[items.LongLength];
+        Array.Fill(result, -1L);
+        for (long i = 0; i < items.LongLength; i++)
+        {
+            T item = items[i];
+            if(LongContains(src, item,out long idx)) result[i]=idx;
+        }
+        return result;
+    }
+    /// <summary>
+    /// get the index in <paramref name="item"/> that size in <see cref="int"/>
+    /// </summary>
+    /// <param name="src">source of item</param>
+    /// <param name="item">the item that will find index in <paramref name="src"/></param>
+    /// <returns>a <see cref="int"/>(value >= 0) about <paramref name="item"/>'s index of <paramref name="src"/>,otherwise <see langword="-1"/></returns>
+    /// <seealso cref="IndexesOf"/>
+    public static int IndexOf<T>(this IEnumerable<T> src, ref T item)
+    {
+        IEnumerator<T> enumerator = src.GetEnumerator();
+        enumerator.MoveNext();
+        int index = 0; bool found = false;
+        do
+        {
+            if (object.ReferenceEquals(item, enumerator.Current)) break;
+            index++;
+        }
+        while (enumerator.MoveNext());
+        return found ? index : -1;
+    }
+    public static int[] IndexesOf<T>(this IEnumerable<T> src, T[] items)
+    {
+        int[] result = new int[items.Length];
+        Array.Fill(result, -1);
+        for (long i = 0; i < items.Length; i++)
+        {
+            T item = items[i];
+            if (Contains(src, item, out int idx)) result[i] = idx;
+        }
+        return result;
+    }
+    public static bool Contains<T>(this IEnumerable<T> source, T item,out int index)
+    {
+        ObjectUtil.ThrowIfNull(source, nameof(source));
+        ObjectUtil.ThrowIfNull(item, nameof(item));
+        IEnumerator<T> enumerator = source.GetEnumerator();
+        enumerator.MoveNext();
+        index = 0;
+        do
+        {
+            if (object.ReferenceEquals(enumerator.Current, item)) return true;
+            index++;
+        }
+        while (enumerator.MoveNext());
+        index = -1;
+        return false;
+    }
+    public static bool LongContains<T>(this IEnumerable<T> source, T item,out long index)
+    {
+        ObjectUtil.ThrowIfNull(source, nameof(source));
+        ObjectUtil.ThrowIfNull(item, nameof(item));
+        IEnumerator<T> enumerator = source.GetEnumerator();
+        enumerator.MoveNext();
+        index = 0;
+        do
+        {
+            if (object.ReferenceEquals(enumerator.Current, item)) return true;
+            index++;
+        }
+        while (enumerator.MoveNext());
+        index = -1;
+        return false;
     }
 }
